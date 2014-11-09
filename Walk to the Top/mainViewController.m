8a928d73fbb56d84fa0767877968898a001400ec
Kevin Chan <kchan039@gmail.com>
2014-11-09 07:09:17 -0800

    Included a selection screen.

--- Walk to the Top/mainViewController.m
+++ Walk to the Top/mainViewController.m
@@ -7,7 +7,11 @@
 //
 
 #import "mainViewController.h"
+#import "LogInControllerViewController.h"
+#import "mainScreenVC.h"
 #import <FacebookSDK/FacebookSDK.h>
+
+BOOL loginOK = NO; // you determine this
 
 @interface mainViewController ()
 
@@ -24,27 +28,57 @@
     [self.view addSubview:loginView];
 }
 
+
+- (BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
+    BOOL segueShouldOccur = NO; // you determine this
+    
+    if ([identifier isEqualToString:@"toMainPage"]) {
+        // perform your computation to determine whether segue should occur
+        
+        if ( loginOK == true){
+            segueShouldOccur = true;
+        }
+        else{
+            segueShouldOccur = false;
+        }
+        
+        
+        if (!segueShouldOccur) {
+            
+            // prevent segue from occurring
+            return NO;
+        }
+    }
+    
+    // by default perform the segue transition
+    return YES;
+}
+
 - (IBAction)checkLogin:(id)sender {
     NSString *username = [self.usernameTextField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
     NSString *password = [self.passwordTextField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
     
     if ([username length] == 0 || [password length] == 0) {
-        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
-                                                            message:@"You have to enter a username and password"
-                                                           delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-        [alertView show];
+        
+        loginOK = false;
+        
     }
+    
     else {
         
         [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser *user, NSError *error) {
             if (error) {
                 UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
-                                                                    message:[error.userInfo objectForKey:@"error"]
-                                                                   delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-                [alertView show];
+                                message:[error.userInfo objectForKey:@"error"]
+                                delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                                [alertView show];
+                loginOK = false;
             }
             else {
                 [self.navigationController popToRootViewControllerAnimated:YES];
+                
+                NSLog(@"(Button was pressed");
+                loginOK = true;
             }
         }];
     }
