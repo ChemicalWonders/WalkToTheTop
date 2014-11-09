5e4d622ec5fd4bdcce5c78e45ea35180e8c8ed2f
Alyza Malunao <amalu001@ucr.edu>
2014-11-08 16:04:47 -0800

    Added LogInControllerViewController, Kevin napping, david prepare for datum

---
+++ Walk to the Top/LogInControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogInControllerViewController.h
+//  Walk to the Top
+//
+//  Created by Alyza Malunao on 11/8/14.
+//  Copyright (c) 2014 Step By Step. All rights reserved.
+//
+
+#import "mainViewController.h"
+
+@interface LogInControllerViewController : mainViewController
+
+@end
